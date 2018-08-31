f596fa517516ac599b8bba3298c84c9dcd07ed0a
Nikita <Happyskalolaz@outlook.com>
2018-08-31 18:58:12 +0500

    Add files via upload

---
+++ java/DrawThread.java
@@ -0,0 +1,102 @@
+package magtu.com.example.clockanalog;
+
+import android.annotation.SuppressLint;
+import android.graphics.*;
+import android.hardware.SensorEventListener;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+import java.util.Random;
+
+/**
+ * Main class of all graphics with thread
+ */
+public class DrawThread extends Thread {
+
+    private DisplayMetrics metrics;
+    private final SurfaceHolder surfaceHolder;
+    private long prevTime;
+    private Paint paint;
+    private boolean runFlag;
+
+    @SuppressLint("ClickableViewAccessibility")
+    DrawThread(SurfaceHolder surfaceHolder, DisplayMetrics metrics) {
+        this.surfaceHolder = surfaceHolder;
+        this.metrics = metrics;
+        // Paint
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        // load components for graphics
+
+        // save current time
+        prevTime = System.currentTimeMillis();
+    }
+
+    void setRunning(boolean run) {
+        runFlag = run;
+    }
+
+    @Override
+    public void run() {
+        Canvas canvas;
+        while (!runFlag) {
+            try {
+                surfaceHolder.wait();
+            } catch (InterruptedException ignored) {
+            }
+        }
+        while (runFlag) {
+            // get current time and calculate difference with older time
+            long now = System.currentTimeMillis();
+            long elapsedTime = now - prevTime;
+            // 1 - second = 1000 milliseconds
+            //  60 frames per second = (1/60s) * 1000ms ~ 16.6ms
+            if (elapsedTime > 16) {
+                //if time > N milliseconds, save current time
+                prevTime = now;
+                //updateFrame(); // picture update with 60 fps (only for moving objects)
+            }
+            canvas = null;
+            try {
+                // get Canvas and create drawings
+                canvas = surfaceHolder.lockCanvas(null);
+                if (canvas != null) synchronized (surfaceHolder) {
+                    // graphics
+                    canvas.drawColor(Color.BLACK);
+
+                }
+            } finally {
+                if (canvas != null) {
+                    // if graphics done, set it on display
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }
+    }
+
+    private void updateFrame() {
+        // supporting method for graphics update
+        // only for moving objects
+    }
+
+    /**
+     * This function is remapping line value
+     * Example:
+     * 0 from 10
+     * remap(4 from 0  to 10) = 40 from 0 to 100
+     *
+     * @param s  value to find
+     * @param a1 start value 1
+     * @param a2 stop value 1
+     * @param b1 start value 2
+     * @param b2 start value 2
+     * @return returns a float value
+     */
+    private float reMap(float s, float a1, float a2, float b1, float b2) {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+
+}
+
+
---
+++ java/MainActivity.java
@@ -0,0 +1,85 @@
+package magtu.com.example.clockanalog;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import magtu.com.example.clockanalog.R;
+
+public class MainActivity extends AppCompatActivity /*implements SensorEventListener*/ {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private final float ALPHA = 0.1f;
+    private float[] accelValues;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        /*mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        assert mSensorManager != null;
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST);*/
+
+        hideUI();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        hideUI();
+       // mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideUI();
+        /*mSensorManager.registerListener(this, mAccelerometer,
+                SensorManager.SENSOR_DELAY_GAME);*/
+    }
+
+    /**
+     * Function that turn on immersive mode
+     */
+    public void hideUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    /*@Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            accelValues = lowPass(sensorEvent.values.clone(), accelValues);
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }*/
+
+    // Low-pass smooth filtering for accelerometer
+    /*protected float[] lowPass(float[] input, float[] output) {
+        if (output == null) return input;
+
+        for (int i = 0; i < input.length; i++) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
+    }*/
+}
